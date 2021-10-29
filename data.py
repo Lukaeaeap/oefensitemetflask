2ba9e094d67c77b168dc677abeb77cc0474bb790
Luuk <lukasrobinhoekstra@gmail.com>
2021-10-29 13:13:46 +0200

    Epic code added to github

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article Uno',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vitae neque voluptatum ipsam ducimus expedita necessitatibus animi voluptas. Facilis quisquam odio neque vitae reprehenderit labore quos corporis beatae earum maxime.',
+            'author': 'Luuk Hoekstra',
+            'create_date': '10-02-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vitae neque voluptatum ipsam ducimus expedita necessitatibus animi voluptas. Facilis quisquam odio neque vitae reprehenderit labore quos corporis beatae earum maxime.',
+            'author': 'Steve Rogers',
+            'create_date': '10-02-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vitae neque voluptatum ipsam ducimus expedita necessitatibus animi voluptas. Facilis quisquam odio neque vitae reprehenderit labore quos corporis beatae earum maxime.',
+            'author': 'Tony Stark',
+            'create_date': '10-02-2021'
+        },
+        {
+            'id': 4,
+            'title': 'Nog een artikel',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vitae neque voluptatum ipsam ducimus expedita necessitatibus animi voluptas. Facilis quisquam odio neque vitae reprehenderit labore quos corporis beatae earum maxime.',
+            'author': 'Anbiya Popal',
+            'create_date': '10-05-2021'
+        }
+    ]
+    return articles
